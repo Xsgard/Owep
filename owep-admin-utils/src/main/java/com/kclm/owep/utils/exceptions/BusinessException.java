256163576fe72b3d95ac49fad22c5917adc6a6cf
asgard <1792181243@qq.com>
2023-08-29 15:20:13 +0800

    Utils

---
+++ owep-admin-utils/src/main/java/com/kclm/owep/utils/exceptions/BusinessException.java
@@ -0,0 +1,37 @@
+package com.kclm.owep.utils.exceptions;
+
+/**
+ * @author Asgard
+ * @version 1.0
+ * @description: 自定义业务异常
+ * @date 2023/8/3 8:54
+ */
+public class BusinessException extends RuntimeException {
+    private String msg;
+    private Integer code = 500;
+
+    public BusinessException(String message) {
+        super(message);
+        this.msg = message;
+    }
+
+    public BusinessException(Integer code, String message) {
+        super(message);
+        this.msg = message;
+        this.code = code;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+
+}
