c0d5d4c47724f88d139f677bc270a78b07fafec6
asgard <1792181243@qq.com>
2023-09-01 17:35:33 +0800

    ProfessionMapper

---
+++ owep-admin-web-dao/src/main/java/com/kclm/owep/mapper/ProfessionMapper.java
@@ -0,0 +1,21 @@
+package com.kclm.owep.mapper;
+
+import com.kclm.owep.entity.Profession;
+import com.kclm.owep.mapper.common.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * @author Asgard
+ * @version 1.0
+ * @description: ProfessionMapper
+ * @date 2023/9/1 13:37
+ */
+@Mapper
+public interface ProfessionMapper extends BaseMapper<Profession> {
+    @Select("select id,prof_name,institute_name from t_profession where is_delete=1 " +
+            "and prof_status=1 ;")
+    List<Profession> getProfessionInfo();
+}
