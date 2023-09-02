6b21a9315fbe43ebb7383c214e556c04582b4b98
asgard <1792181243@qq.com>
2023-09-02 09:27:12 +0800

    UserController 添加-> 学生bsSuggest请求 添加-> 方法注释

--- owep-admin-web-controller/src/main/java/com/kclm/owep/web/controller/UserController.java
+++ owep-admin-web-controller/src/main/java/com/kclm/owep/web/controller/UserController.java
@@ -3,6 +3,7 @@ package com.kclm.owep.web.controller;
 import com.itextpdf.text.pdf.PRIndirectReference;
 import com.kclm.owep.dto.NodeDTO;
 import com.kclm.owep.dto.StudentDTO;
+import com.kclm.owep.dto.StudentSuggestDTO;
 import com.kclm.owep.entity.Student;
 import com.kclm.owep.entity.User;
 import com.kclm.owep.service.StudentService;
@@ -31,29 +32,33 @@ public class UserController {
     @Autowired
     private StudentService studentService;
 
+    //跳转至管理员管理页面
     @GetMapping("/adminList")
     public String toAdminList() {
         return "user/adminList";
     }
 
+    //跳转至学生管理页面
     @GetMapping("/stuList")
     public String toStuList() {
         return "user/stuList";
     }
 
+    //管理员-获取全部信息
     @ResponseBody
     @GetMapping("/adminList/getTable")
     public R getAdminList(Integer limit, Integer offset) {
         return R.ok().put("data", userService.getAllAdmin(limit, offset));
     }
 
+    //管理员-搜索
     @GetMapping("/adminList/search")
     @ResponseBody
     public R getUserByNameAndRealName(User user) {
         return R.ok().put("data", userService.selectUserByCond(user));
     }
 
-
+    //学生-获取全部信息
     @GetMapping("/stuList/getTable")
     @ResponseBody
     public R getStudentList(String order, Integer limit, Integer offset) {
@@ -61,6 +66,7 @@ public class UserController {
         return R.ok().put("data", studentInfo);
     }
 
+    //学生-添加
     @PostMapping("/stuList/addStudent")
     @ResponseBody
     public R addStudent(@RequestBody Student student) {
@@ -72,6 +78,7 @@ public class UserController {
         return R.ok("添加成功！");
     }
 
+    //学生-修改
     @PostMapping("/stuList/edit")
     @ResponseBody
     public R editStudent(@RequestBody Student student) {
@@ -83,6 +90,7 @@ public class UserController {
         return R.ok("修改成功！");
     }
 
+    //学生-状态修改
     @GetMapping("/stuList/switch")
     @ResponseBody
     public R stuSwitch(Integer id, Integer status) {
@@ -90,13 +98,23 @@ public class UserController {
         return R.ok("修改成功！");
     }
 
+    //学生-学生班级树
     @GetMapping("/stuList/classTreeCheck")
     @ResponseBody
     public R getClassTreeCheck(Integer id) {
-        List<NodeDTO> clazzTreeCheck = studentService.getClazzTreeCheck();
+        List<NodeDTO> clazzTreeCheck = studentService.getClazzTreeCheck(id);
         return R.ok().put("data", clazzTreeCheck);
     }
 
+    //学生-bsSuggest请求
+    @GetMapping("/stuList/suggest")
+    @ResponseBody
+    public R studentSuggestInfo() {
+        List<StudentSuggestDTO> studentSuggestInfo = studentService.getStudentSuggestInfo();
+        return R.ok().put("value", studentSuggestInfo);
+    }
+
+    //管理员-修改状态
     @GetMapping("/adminList/switch")
     @ResponseBody
     public R activeUser(Integer userId, Integer status) {
@@ -108,6 +126,7 @@ public class UserController {
         return R.ok("状态修改成功！");
     }
 
+    //管理员-添加
     @PostMapping("/adminList/addAdmin")
     @ResponseBody
     public R addUser(@RequestBody User user) {
@@ -117,9 +136,9 @@ public class UserController {
             return R.error(e.getMessage());
         }
         return R.ok("添加成功！");
-
     }
 
+    //管理员-修改
     @PostMapping("/adminList/edit")
     @ResponseBody
     public R editUser(@RequestBody User user) {
@@ -131,6 +150,7 @@ public class UserController {
         return R.ok("修改成功！");
     }
 
+    //管理员-删除
     @GetMapping("/adminList/delete")
     @ResponseBody
     public R deleteById(Integer id) {
@@ -142,6 +162,7 @@ public class UserController {
         return R.ok("修改成功！");
     }
 
+    //管理员-管理员用户组信息
     @GetMapping("/adminList/treeCheck")
     @ResponseBody
     public R getUserGroup(Integer id) {
@@ -149,6 +170,7 @@ public class UserController {
         return R.ok().put("data", userGroup);
     }
 
+    //管理员-批量删除管理员
     @PostMapping("/adminList/deleteByGroup")
     @ResponseBody
     public R deleteByIds(@RequestBody List<Serializable> ids) {
@@ -160,6 +182,7 @@ public class UserController {
         return R.ok("删除成功！");
     }
 
+    //管理员-导出管理员信息
     @GetMapping("/adminList/exportUserInfo")
     @ResponseBody
     public R exportUserInfo(HttpServletResponse response) {
