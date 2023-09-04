5b6c19cb5dc8fd4fb8590740c8a5d231ac7b1899
asgard <1792181243@qq.com>
2023-09-04 18:58:59 +0800

    UploadImg

---
+++ owep-admin-utils/src/main/java/com/kclm/owep/utils/util/UploadImg.java
@@ -0,0 +1,43 @@
+package com.kclm.owep.utils.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * @author Asgard
+ * @version 1.0
+ * @description: 上传图片
+ * @date 2023/8/14 19:09
+ */
+@Slf4j
+public class UploadImg {
+    private UploadImg() {
+    }
+
+    public static String uploadImg(MultipartFile avatarFile, String savePath) {
+        String filename = avatarFile.getOriginalFilename();//获取文件名以及后缀名
+        String projectPath = System.getProperty("user.dir");
+        String dirPath = projectPath + savePath;
+        log.info(dirPath);
+        File filePath = new File(dirPath);
+        if (!filePath.exists())
+            filePath.mkdirs();
+        UUID uuid = UUID.randomUUID();//生成文件名
+        String newFileName = null;//生成文件名+后缀
+        if (filename != null) {
+            newFileName = uuid + filename.substring(filename.lastIndexOf("."));
+        }
+        try {
+            log.info(dirPath + newFileName);
+            avatarFile.transferTo(new File(dirPath + newFileName));
+        } catch (IOException e) {
+            log.debug(e.getMessage());
+            return null;
+        }
+        return newFileName;
+    }
+}
