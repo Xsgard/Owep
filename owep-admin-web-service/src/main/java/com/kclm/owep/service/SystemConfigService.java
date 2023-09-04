da79672f1e33449e998db29276ea3f2ea2ff7c8f
asgard <1792181243@qq.com>
2023-09-04 13:33:50 +0800

    SystemConfigService

---
+++ owep-admin-web-service/src/main/java/com/kclm/owep/service/SystemConfigService.java
@@ -0,0 +1,11 @@
+package com.kclm.owep.service;
+
+/**
+ * @author Asgard
+ * @version 1.0
+ * @description: SystemService
+ * @date 2023/9/4 13:09
+ */
+public interface SystemConfigService {
+
+}
---
+++ owep-admin-web-service/src/main/java/com/kclm/owep/service/impl/SystemConfigServiceImpl.java
@@ -0,0 +1,10 @@
+package com.kclm.owep.service.impl;
+
+/**
+ * @author Asgard
+ * @version 1.0
+ * @description: SystemServiceImpl
+ * @date 2023/9/4 13:09
+ */
+public class SystemConfigServiceImpl {
+}
