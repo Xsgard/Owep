a6c5a5a0455b88be992498bd948e218683603503
asgard <1792181243@qq.com>
2023-09-04 13:32:57 +0800

    SystemConfigMapper

---
+++ owep-admin-web-dao/src/main/java/com/kclm/owep/mapper/SystemConfigMapper.java
@@ -0,0 +1,19 @@
+package com.kclm.owep.mapper;
+
+import com.kclm.owep.entity.SystemConfig;
+import com.kclm.owep.mapper.common.BaseMapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * @author Asgard
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/9/4 13:06
+ */
+public interface SystemConfigMapper extends BaseMapper<SystemConfig> {
+
+    @Select("select * from t_system_config;")
+    List<SystemConfig> getSystemConfig();
+}
