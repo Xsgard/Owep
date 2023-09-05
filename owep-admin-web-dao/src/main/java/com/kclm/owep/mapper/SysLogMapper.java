3aa5d0a64d98b58d0de7026b556b30a1567c28bc
asgard <1792181243@qq.com>
2023-09-05 15:23:32 +0800

    SysLogMapper

--- owep-admin-web-dao/src/main/java/com/kclm/owep/mapper/SysLogMapper.java
+++ owep-admin-web-dao/src/main/java/com/kclm/owep/mapper/SysLogMapper.java
@@ -3,10 +3,10 @@ package com.kclm.owep.mapper;
 import com.kclm.owep.entity.SystLog;
 import com.kclm.owep.mapper.common.BaseMapper;
 import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -22,4 +22,8 @@ public interface SysLogMapper extends BaseMapper<SystLog> {
 
     @Delete("delete from t_syslog where id=#{id};")
     int deleteById(Integer id);
+
+    @Select("select * from t_syslog where login_user_name=#{username} and" +
+            " (create_time between #{start} and #{end})")
+    List<SystLog> search(String username, LocalDate start, LocalDate end);
 }
