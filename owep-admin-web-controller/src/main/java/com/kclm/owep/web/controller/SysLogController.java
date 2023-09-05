c688cd73266dc2c971366f82bb7e3d61ad94d46f
asgard <1792181243@qq.com>
2023-09-05 15:23:18 +0800

    SysLogController 添加-》 日志搜索功能 添加-》 方法注释

--- owep-admin-web-controller/src/main/java/com/kclm/owep/web/controller/SysLogController.java
+++ owep-admin-web-controller/src/main/java/com/kclm/owep/web/controller/SysLogController.java
@@ -1,8 +1,6 @@
 package com.kclm.owep.web.controller;
 
 import com.kclm.owep.dto.LogTableDTO;
-import com.kclm.owep.dto.SystLogDTO;
-import com.kclm.owep.entity.SystLog;
 import com.kclm.owep.service.SysLogService;
 import com.kclm.owep.utils.exceptions.BusinessException;
 import com.kclm.owep.utils.exceptions.ParameterWrongException;
@@ -11,10 +9,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.annotation.Resource;
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -31,11 +30,13 @@ public class SysLogController {
     @Autowired
     private SysLogService sysLogService;
 
+    //跳转至日志页面
     @GetMapping("/list")
-    public String toLogListPage(){
+    public String toLogListPage() {
         return "log/list";
     }
 
+    //获取日志集合
     @GetMapping("/sysLogs")
     @ResponseBody
     public R getLogs(Integer offset, Integer limit) {
@@ -43,6 +44,7 @@ public class SysLogController {
         return R.ok().put("data", sysLogs);
     }
 
+    //删除
     @GetMapping("/delete")
     @ResponseBody
     public R deleteById(Integer id) {
@@ -54,5 +56,21 @@ public class SysLogController {
             return R.error(e.getMessage());
         }
         return R.ok("删除成功！");
+    }
+
+    //查询
+    @PostMapping("/search")
+    @ResponseBody
+    public R search(String username, String startTime, String endTime) {
+        LocalDate start = LocalDate.parse(startTime);
+        LocalDate end = LocalDate.parse(endTime);
+        try {
+            List<LogTableDTO> search = sysLogService.search(username, start, end);
+            return R.ok().put("data", search);
+        } catch (ParameterWrongException e) {
+            return R.error(e.getMsg());
+        } catch (BusinessException e) {
+            return R.error(e.getMessage());
+        }
     }
 }
