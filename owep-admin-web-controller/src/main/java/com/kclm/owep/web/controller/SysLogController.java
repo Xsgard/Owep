8d9bbd469e85fcca5403c698974e853633e48b36
asgard <1792181243@qq.com>
2023-09-05 11:32:58 +0800

    SysLogController 添加-》 分页获取日志信息 添加-》 根据Id删除日志信息

---
+++ owep-admin-web-controller/src/main/java/com/kclm/owep/web/controller/SysLogController.java
@@ -0,0 +1,53 @@
+package com.kclm.owep.web.controller;
+
+import com.kclm.owep.dto.LogTableDTO;
+import com.kclm.owep.dto.SystLogDTO;
+import com.kclm.owep.entity.SystLog;
+import com.kclm.owep.service.SysLogService;
+import com.kclm.owep.utils.exceptions.BusinessException;
+import com.kclm.owep.utils.exceptions.ParameterWrongException;
+import com.kclm.owep.utils.util.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author Asgard
+ * @version 1.0
+ * @description: SysLogController
+ * @date 2023/9/4 19:01
+ */
+@Controller
+@RequestMapping("/log")
+@Slf4j
+public class SysLogController {
+
+    @Autowired
+    private SysLogService sysLogService;
+
+    @GetMapping("/sysLogs")
+    @ResponseBody
+    public R getLogs(Integer offset, Integer limit) {
+        List<LogTableDTO> sysLogs = sysLogService.getSysLogs(offset, limit);
+        return R.ok().put("data", sysLogs);
+    }
+
+    @GetMapping("/delete")
+    @ResponseBody
+    public R deleteById(Integer id) {
+        try {
+            sysLogService.deleteById(id);
+        } catch (ParameterWrongException e) {
+            return R.error(e.getMsg());
+        } catch (BusinessException e) {
+            return R.error(e.getMessage());
+        }
+        return R.ok("删除成功！");
+    }
+}
